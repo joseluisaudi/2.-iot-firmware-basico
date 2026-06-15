02b3d475a19553dbe0bfc0909210f07b51e689ac
joseluisaudi <joseluisastudillo301@gmail.com>
2026-06-15 11:04:32 -0500

    refactor: comentar include de Arduino.h con nota aclaratoria

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include <Arduino.h>
+// #include <Arduino.h>
 
 void setup() {
   // Initialize serial communication at 115200 baud
   Serial.begin(115200);
   delay(1000); // Give the serial monitor time to connect
-  
+
   Serial.println("=====================================");
   Serial.println("Hello World from ESP32!");
   Serial.println("=====================================");
